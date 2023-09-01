c1e9df0b3e87b62b14c90e6c6e331cdcdc083609
theo2302 <131917755+theo2302@users.noreply.github.com>
2023-09-01 19:34:55 -0300

    Update panda.py

--- panda.py
+++ panda.py
@@ -9,17 +9,17 @@ warnings.filterwarnings('ignore')
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # Define the Streamlit app title
-st.title("CSV DataFrame Creator")
+st.title("Analisador de Tabelas")
 
 # Define a delimiter selection widget with labels
 delimiter_options = {',': ',', ';': ';', '\t': 'Tab', ' ': 'Space', '|': '|'}
-delimiter = st.selectbox("Select the CSV delimiter:", list(delimiter_options.values()))
+delimiter = st.selectbox("Selecione o delimitador usado:", list(delimiter_options.values()))
 
 # Get the actual delimiter character based on the selected label
 selected_delimiter = [k for k, v in delimiter_options.items() if v == delimiter][0]
 
 # Create a file upload widget
-uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
+uploaded_file = st.file_uploader("Upload de arquivos CSV", type=["csv"])
 
 # Check if a file has been uploaded
 if uploaded_file is not None:
@@ -33,35 +33,34 @@ if uploaded_file is not None:
     df = pd.read_csv(data, delimiter=delimiter)
 
     # Display the DataFrame
-    st.write("Uploaded DataFrame:", df)
+    st.write("DataFrame:", df)
 
     # You can perform various operations on the DataFrame here
     # For example, you can display a summary of the DataFrame:
-    st.write("Information", df.info())
     
-    st.write("DataFrame Summary:", df.describe())
+    st.write("Resumo estatistico:", df.describe())
 
-    st.subheader("Correlation Heatmap")
+    st.subheader("Heatmap")
     plt.figure()
     sns.heatmap(df.corr(numeric_only=True), annot=False, linewidth=.5, cmap='Blues', vmin=-1, vmax=1)
     st.pyplot(plt)
 
-    st.header("Data Manipulation")
+    st.header("Agrupamento de dados")
 
 # Check if a DataFrame has been created
 
     # Create a checkbox for grouping by columns
-    groupby_columns = st.multiselect("Select columns to group by:", df.columns)
+    groupby_columns = st.multiselect("Selecione Colunas:", df.columns)
 
     if groupby_columns:
         # Create a dropdown for selecting an aggregate function
-        aggregate_function = st.selectbox("Select an aggregate function:", ["mean", "sum", "min", "max"])
+        aggregate_function = st.selectbox("Selecione uma função agregadora:", ["mean", "sum", "min", "max"])
 
         # Group the DataFrame by the selected columns and apply the aggregate function
         grouped_df = df.groupby(groupby_columns).agg(aggregate_function)
 
         # Display the grouped DataFrame
-        st.write("Grouped DataFrame:", grouped_df)
+        st.write("DataFrame agrupado:", grouped_df)
 
 # Optionally, you can add other widgets and interact with the DataFrame further.
 
